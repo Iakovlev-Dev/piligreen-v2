45d07b6b5f2d95699f601f8f747f52c5c52bd102
Iakovlev Igor <iakovlev.igor.dev@gmail.com>
2025-01-13 23:42:26 +0300

    add main section fix

--- src/pages/page-main/page-main.tsx
+++ src/pages/page-main/page-main.tsx
@@ -6,6 +6,7 @@ export default function PageMain () {
         <>
             <Header />
             <main className="main">
+
                 <section className="container">
                     <div className="main-section">
                         <div className="main_content">
