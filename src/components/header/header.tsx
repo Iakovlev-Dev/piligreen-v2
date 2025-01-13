e2dc34b3eb4a4726f0c3ec0ec791b457ac8d2a5e
Iakovlev Igor <iakovlev.igor.dev@gmail.com>
2025-01-13 23:35:28 +0300

    add main section

---
+++ src/components/header/header.tsx
@@ -0,0 +1,18 @@
+import {Container, Nav, Navbar} from "react-bootstrap";
+
+export default function Header () {
+    return (
+        <header>
+            <Navbar bg="light" data-bs-theme="light">
+                <Container>
+                    <Navbar.Brand href="#home" className="">LOGO</Navbar.Brand>
+                    <Nav className="me-auto nav-links">
+                        <Nav.Link href="#pricing">Главная страница</Nav.Link>
+                        <Nav.Link href="#home">Наша продукция</Nav.Link>
+                        <Nav.Link href="#features">Контакты</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
--- src/pages/page-main/page-main.tsx
+++ src/pages/page-main/page-main.tsx
@@ -1,5 +1,33 @@
+import Header from "../../components/header/header.tsx";
+import {Button} from "react-bootstrap";
+
 export default function PageMain () {
     return (
-        <h1>Hello </h1>
+        <>
+            <Header />
+            <main className="main">
+                <section className="container">
+                    <div className="main-section">
+                        <div className="main_content">
+                            <div className="main_container">
+                                <h1 className="main_h1">Производитель деревянной упаковочной тары</h1>
+                                <p className="main_p">Компания "Пилигрин" находится на передовом крае производства
+                                    деревянной упаковки в Санкт-Петербурге и Ленинградской области.
+                                    Наша приверженность качеству и инновациям гарантирует,
+                                    что мы удовлетворяем разнообразные потребности наших клиентов.
+                                </p>
+                                <div className="main_buttons">
+                                    <Button variant="dark" href="#">Узнать больше</Button>
+                                    <Button variant="light" href="#">Контакты</Button>
+                                </div>
+                            </div>
+                        </div>
+                        <img className="main_img" alt="main-photo" src="/img/PlaceholderImage.jpg"/>
+                    </div>
+                </section>
+
+            </main>
+        </>
+
     )
 }
